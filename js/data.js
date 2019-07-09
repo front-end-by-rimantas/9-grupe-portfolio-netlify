8739f22dfd0208f838e0a301e105799c6677e8d6
EmilisJ <45522661+EmilisJ@users.noreply.github.com>
2019-07-09 14:28:55 +0300

    conflict prevention and background colors

--- js/data.js
+++ js/data.js
@@ -1,1 +1,25 @@
 "use strict";
+
+/* Hero */
+
+/* Header Nav BAr */
+
+/* About me */
+
+/* My skills */
+
+/* My resume */
+
+/* Achievements */
+
+/* My services */
+
+/* My portfolio */
+
+/* Testimonials */
+
+/* Latest Blog */
+
+/* Get in touch */
+
+/* Footer */
